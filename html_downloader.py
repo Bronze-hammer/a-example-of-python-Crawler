9125d315a9096bfc3c41aaca789a7f49c5b58ab4
Bronze-hammer <zihui_xu@126.com>
2017-06-30 14:54:51 +0800

    Update html_downloader.py

--- html_downloader.py
+++ html_downloader.py
@@ -7,10 +7,13 @@ class HtmlDownloader(object):
     def download(self, url):
         if url is None:
             return None
-
+        
+        #直接请求
         response = urllib2.urlopen(url)
 
+        #获取状态吗，如果是200则表示获取成功
         if response.getcode() != 200:
             return None
 
+        #读取内容
         return response.read()
